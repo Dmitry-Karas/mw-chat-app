76081360f763c92b7f04a7e372e7fef0a7f1c098
Dmitry <dmitrythekaras@gmail.com>
2021-10-14 14:06:30 +0300

    refactor(models): delete comments, add min length decorator

--- server/models/Message.ts
+++ server/models/Message.ts
@@ -4,7 +4,7 @@ import {
   Column,
   CreateDateColumn,
 } from "typeorm";
-import { IsDefined, MaxLength } from "class-validator";
+import { IsDefined, MaxLength, MinLength } from "class-validator";
 
 @Entity("messages")
 export class Message {
@@ -25,6 +25,7 @@ export class Message {
 
   @Column()
   @IsDefined()
+  @MinLength(1)
   @MaxLength(200)
   body: string;
 
@@ -35,32 +36,3 @@ export class Message {
   @CreateDateColumn()
   createdAt: Date;
 }
-
-// import { Schema, model } from "mongoose";
-
-// interface IMessage {
-//   _id: string;
-//   userId: string;
-//   sender: string;
-//   body: string;
-//   time: string;
-//   createdAt: Date;
-// }
-
-// const messageSchema = new Schema(
-//   {
-//     userId: { type: String, required: true },
-//     sender: { type: String, required: true },
-//     body: { type: String, required: true, maxlength: 200 },
-//     time: { type: String, required: true },
-//   },
-//   {
-//     versionKey: false,
-//     timestamps: {
-//       createdAt: true,
-//       updatedAt: false,
-//     },
-//   }
-// );
-
-// export const Message = model<IMessage>("Message", messageSchema);
